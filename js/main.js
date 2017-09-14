78a5d6be17e471c566fdf51a6dd6813f99a95986
George Antonious <george@antonious.ca>
2017-09-14 01:50:54 -0600

    Make it nice and green

--- js/main.js
+++ js/main.js
@@ -2,21 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     startCursorBlinking()
 });
 
-function getBackgroundColor() {
-    return "white"
-}
-
-function getCursorColor() {
-    return "black"
-}
-
 function startCursorBlinking() {
     var isCursorVisbile = true
     setInterval(function() {
         if (isCursorVisbile) {
-            $("#cursor").css("color", getBackgroundColor())
+            $("#cursor").css("visibility", "hidden")
         } else {
-            $("#cursor").css("color", getCursorColor())
+            $("#cursor").css("visibility","visible")
         }
         isCursorVisbile = !isCursorVisbile
     }, 600)
