b89e725cb86388ee7a8c1e49aa530cd784760db0
Arjun Kalburgi <askalburgi@gmail.com>
2017-09-27 16:58:25 -0600

    changing background colours easter egg

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     startCursorBlinking()
+    chooseBackgroundColour()
 });
 
 function startCursorBlinking() {
@@ -12,4 +13,23 @@ function startCursorBlinking() {
         }
         isCursorVisbile = !isCursorVisbile
     }, 600)
+}
+
+function chooseBackgroundColour() {
+    var colour = Math.floor(Math.random() * (5 - 1) + 1); // four colours
+    console.log(colour); 
+    switch (colour) {
+        case 1: 
+            $("body").css("backgroundColor", "#80B1F5"); 
+            break; 
+        case 2: 
+            $("body").css("backgroundColor", "#94D3A3"); 
+            break; 
+        case 3: 
+            $("body").css("backgroundColor", "#E06D6F"); 
+            break; 
+        case 4: 
+            $("body").css("backgroundColor", "#F1C37F"); 
+            break; 
+    } 
 }
