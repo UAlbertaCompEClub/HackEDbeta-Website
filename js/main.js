cec30a8f85ee8d6e9b6b4f2745c56388f981fbb6
Mohammad Al-Ahdal <malahdal@ualberta.ca>
2017-09-28 15:42:38 -0600

    remove unnecessary CSS, HTML, and JS
    also fix .logo css width

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    startCursorBlinking()
-    chooseBackgroundColour()
+    chooseBackgroundColour();
 });
-
-function startCursorBlinking() {
-    var isCursorVisbile = true
-    setInterval(function() {
-        if (isCursorVisbile) {
-            $("#cursor").css("visibility", "hidden")
-        } else {
-            $("#cursor").css("visibility","visible")
-        }
-        isCursorVisbile = !isCursorVisbile
-    }, 600)
-}
 
 function chooseBackgroundColour() {
     var colour = Math.floor(Math.random() * (5 - 1) + 1); // four colours
