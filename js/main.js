2e6a4dae143cd9c338e09494bc1ca72789fea23c
Mohammad Al-Ahdal <malahdal@ualberta.ca>
2017-09-29 09:51:14 -0600

    really bad way of scrolling stuff (but it works)
    super choppy, too many animations (hence kinda laggy)

--- js/main.js
+++ js/main.js
@@ -34,15 +34,15 @@ function checkScroll(){
         state = 0;
         $("html body").animate({ backgroundColor: "#94D3A3" }, 300);
         $(".paper").animate({ color: "#94D3A3" }, 300);
-    }else if (scrollPercent > 1 && scrollPercent < 2 && state != 1) {
+    }else if (scrollPercent > 1 && scrollPercent < 1.95 && state != 1) {
         state = 1;
         $("html body").animate({ backgroundColor: "#80B1F5" }, 300);
         $(".paper").animate({ color: "#80B1F5" }, 300);
-    }else if (scrollPercent > 2 && scrollPercent < 3 && state != 2) {
+    }else if (scrollPercent > 1.95 && scrollPercent < 2.9 && state != 2) {
         state = 2;
         $("html body").animate({ backgroundColor: "#E06D6F" }, 300);
         $(".paper").animate({ color: "#E06D6F" }, 300);
-    }else if (scrollPercent > 3 && state != 3) {
+    }else if (scrollPercent > 2.9 && state != 3) {
         state = 3;
         $("html body").animate({ backgroundColor: "#F1C37F" }, 300);
         $(".paper").animate({ color: "#F1C37F" }, 300);
@@ -50,9 +50,25 @@ function checkScroll(){
     
     if (state > 0) {
         $(".paper").addClass("dontMove");
+        if (state == 1) {
+            $("#one").animate({ height: 0.95*window.innerHeight-window.scrollY+window.innerHeight,
+                                bottom: window.scrollY - window.innerHeight }, 0.1);
+            
+            $("#two").animate({ bottom: window.scrollY - 1.95*window.innerHeight*0.95 }, 0.1);
+            $("#three").animate({ bottom: window.scrollY - 2.9*window.innerHeight*0.95 }, 0.1);
+        }else if (state == 2) {
+            $("#two").animate({ height: 0.95*window.innerHeight-window.scrollY+1.95*window.innerHeight,
+                                bottom: window.scrollY - 1.95*window.innerHeight}, 0.1);
+            
+            $("#three").animate({ bottom: window.scrollY - 2.9*window.innerHeight*0.95 }, 0.1);
+        }else if (state == 3) {
+            $("#three").animate({ height: 0.95*window.innerHeight-window.scrollY+2.9*window.innerHeight,
+                                bottom: window.scrollY - 2.9*window.innerHeight}, 0.1);
+        }
     }else {
         $(".paper").removeClass("dontMove");
     }
+    
 }
 
 
