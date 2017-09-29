ee708b2a6000fb8df476ace2af3cbeb54b90c27c
Mohammad Al-Ahdal <malahdal@ualberta.ca>
2017-09-29 10:10:18 -0600

    fixed choppy start

--- js/main.js
+++ js/main.js
@@ -30,19 +30,19 @@ function checkScroll(){
     console.log(scrollPercent);
     
     
-    if (scrollPercent < 1 && state != 0) {
+    if (scrollPercent < 0.975 && state != 0) {
         state = 0;
         $("html body").animate({ backgroundColor: "#94D3A3" }, 300);
         $(".paper").animate({ color: "#94D3A3" }, 300);
-    }else if (scrollPercent > 1 && scrollPercent < 1.95 && state != 1) {
+    }else if (scrollPercent > 0.975 && scrollPercent < 1.9 && state != 1) {
         state = 1;
         $("html body").animate({ backgroundColor: "#80B1F5" }, 300);
         $(".paper").animate({ color: "#80B1F5" }, 300);
-    }else if (scrollPercent > 1.95 && scrollPercent < 2.9 && state != 2) {
+    }else if (scrollPercent > 1.9 && scrollPercent < 2.85 && state != 2) {
         state = 2;
         $("html body").animate({ backgroundColor: "#E06D6F" }, 300);
         $(".paper").animate({ color: "#E06D6F" }, 300);
-    }else if (scrollPercent > 2.9 && state != 3) {
+    }else if (scrollPercent > 2.85 && scrollPercent < 3.8 && state != 3) {
         state = 3;
         $("html body").animate({ backgroundColor: "#F1C37F" }, 300);
         $(".paper").animate({ color: "#F1C37F" }, 300);
@@ -54,16 +54,16 @@ function checkScroll(){
             $("#one").animate({ height: 0.95*window.innerHeight-window.scrollY+window.innerHeight,
                                 bottom: window.scrollY - window.innerHeight }, 0.1);
             
-            $("#two").animate({ bottom: window.scrollY - 1.95*window.innerHeight*0.95 }, 0.1);
-            $("#three").animate({ bottom: window.scrollY - 2.9*window.innerHeight*0.95 }, 0.1);
+            $("#two").animate({ bottom: window.scrollY - 2*window.innerHeight*0.95 }, 0.1);
+            $("#three").animate({ bottom: window.scrollY - 3*window.innerHeight*0.95 }, 0.1);
         }else if (state == 2) {
-            $("#two").animate({ height: 0.95*window.innerHeight-window.scrollY+1.95*window.innerHeight,
-                                bottom: window.scrollY - 1.95*window.innerHeight}, 0.1);
+            $("#two").animate({ height: 0.95*window.innerHeight-window.scrollY+1.9*window.innerHeight,
+                                bottom: window.scrollY - 2.05*window.innerHeight}, 0.1);
             
-            $("#three").animate({ bottom: window.scrollY - 2.9*window.innerHeight*0.95 }, 0.1);
+            $("#three").animate({ bottom: window.scrollY - 3*window.innerHeight*0.95 }, 0.1);
         }else if (state == 3) {
-            $("#three").animate({ height: 0.95*window.innerHeight-window.scrollY+2.9*window.innerHeight,
-                                bottom: window.scrollY - 2.9*window.innerHeight}, 0.1);
+            $("#three").animate({ height: 0.95*window.innerHeight-window.scrollY+3*window.innerHeight,
+                                bottom: window.scrollY - 3*window.innerHeight}, 0.1);
         }
     }else {
         $(".paper").removeClass("dontMove");
