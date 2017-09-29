19c8ae0b60d4fe64da11fb4cb859a18b31574e48
Mohammad Al-Ahdal <malahdal@ualberta.ca>
2017-09-29 17:44:33 -0600

    add a secondary hacked logo and added color variables for JS and CSS

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 var scrollPercent; // Scrolled  Y / Page height
 var state; // Dependent on page, 0 is no page and 3 is last page (more can be added easily)
-
+///*
+var BLUE = "#80B1F5";
+var GREEN = "#94D3A3";
+var RED = "#E06D6F";
+var YELLOW = "#F1C37F";
+var DGREEN = "#16703D";
+//*/
 document.addEventListener("DOMContentLoaded", function(event) {
     scrollPercent = window.scrollY / window.innerHeight; //update scroll percent
     
     // Set State and change BG colour on load/refresh
     if (scrollPercent < 1) {
         state = 0;
-        $(".bg").css("backgroundColor", "#94D3A3");
-        //$(".paper").css("color", "#94D3A3");
+        $(".bg").css("backgroundColor", DGREEN);
     }else if (scrollPercent > 0.975 && scrollPercent < 1.825) {
         state = 1;
-        $(".bg").css("backgroundColor", "#80B1F5");
-        //$(".paper").css("color", "#80B1F5");
+        $(".bg").css("backgroundColor", BLUE);
     }else if (scrollPercent > 1.825 && scrollPercent < 2.725) {
         state = 2;
-        $(".bg").css("backgroundColor", "#E06D6F");
-        //$(".paper").css("color", "#E06D6F");
+        $(".bg").css("backgroundColor", RED);
     }else if (scrollPercent > 2.725) {
         state = 3;
-        $(".bg").css("backgroundColor", "#F1C37F");
-        //$(".paper").css("color", "#F1C37F");
+        $(".bg").css("backgroundColor", YELLOW);
     }
 });
 
@@ -32,23 +34,19 @@ function checkScroll(){
     // animate change bg, set state, and animate text change
     if (scrollPercent < 0.975 && state != 0) {
         state = 0;
-        $(".bg").animate({ backgroundColor: "#94D3A3" }, 300);
-        $(".paper").animate({ color: "#94D3A3" }, 300);
+        $(".bg").animate({ backgroundColor: DGREEN }, 300);
         console.log(state);
     }else if (scrollPercent > 0.975 && scrollPercent < 1.9 && state != 1) {
         state = 1;
-        $(".bg").animate({ backgroundColor: "#80B1F5" }, 300);
-        $(".paper").animate({ color: "#80B1F5" }, 300);
+        $(".bg").animate({ backgroundColor: BLUE }, 300);
         console.log(state);
     }else if (scrollPercent > 1.9 && scrollPercent < 2.85 && state != 2) {
         state = 2;
-        $(".bg").animate({ backgroundColor: "#E06D6F" }, 300);
-        $(".paper").animate({ color: "#E06D6F" }, 300);
+        $(".bg").animate({ backgroundColor: RED }, 300);
         console.log(state);
     }else if (scrollPercent > 2.85 && scrollPercent < 3.8 && state != 3) {
         state = 3;
-        $(".bg").animate({ backgroundColor: "#F1C37F" }, 300);
-        $(".paper").animate({ color: "#F1C37F" }, 300);
+        $(".bg").animate({ backgroundColor: YELLOW }, 300);
         console.log(state);
     }
     // check when border needs to be activates
@@ -68,5 +66,6 @@ function checkScroll(){
 #94D3A3 GREEN
 #E06D6F RED
 #F1C37F YELLOW
+#16703D DGREEN
     
 */
