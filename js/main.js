871a37720a495b40c7d5bbc0e7d09b3b8b280bd1
Arjun Kalburgi <askalburgi@users.noreply.github.com>
2017-10-20 10:43:43 -0600

    Mine and Denis nearly completed site :)  (#2)
    * added favicon, cleaned up a bit
    
    * removed unused CSS, unused dependencies
    
    * reorganized CSS a bit
    
    * switched to SCSS instead of CSS (more readable), setup npm for live development
    
    * fixed gitignore / node_modules
    
    * more refactoring, cleaning up
    
    * fixed mobile page slideshow, commented
    
    * adding copy to the website
    
    * header and page section tweaks
    
    * sponsorship code (incomplete)
    
    * sponsor and footer rough work
    
    * added basic FAQ and styles
    
    * Added changing background colours to faq and hover effects on bubble, other css tweaks
    
    * New copy + design tweaks
    
    * LINKS
    
    * moved css code to scss where it should be
    
    * merged stashed changes
    
    * added logo locally, fixed up a bit

--- js/main.js
+++ js/main.js
@@ -1,76 +1,68 @@
-var slide = 1; //current slide
-
-var BLUE = "#80B1F5";
-var GREEN = "#94D3A3";
-var RED = "#E06D6F";
-var YELLOW = "#F1C37F";
-var DGREEN = "#16703D";
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    startCursorBlinking();
-});
-
-
-function slideChange(reqSlide) {
-    // when slide changes, animate the slide to the side
-
-    var moveStr = "-" + String(reqSlide - 1) + "00vw";
-    
-    console.log("reqSlide: " + reqSlide);
-    
-    $(".page").animate({
-        left: moveStr
-    },300);
-    
-    //set slide's new state
-    slide = reqSlide;
-
-    // Adjust active/inactive 
-    if (reqSlide == 1) {
-        $(".section-title:first-child").css("opacity", "1"); 
-        $(".section-title:nth-child(2)").css("opacity", "0.4"); 
-        $(".section-title:nth-child(3)").css("opacity", "0.4"); 
-        $(".section-title:nth-child(4)").css("opacity", "0.4"); 
-    } else if (reqSlide == 2) {
-        $(".section-title:first-child").css("opacity", "0.4"); 
-        $(".section-title:nth-child(2)").css("opacity", "1"); 
-        $(".section-title:nth-child(3)").css("opacity", "0.4"); 
-        $(".section-title:nth-child(4)").css("opacity", "0.4"); 
-    } else if (reqSlide == 3) {
-        $(".section-title:first-child").css("opacity", "0.4"); 
-        $(".section-title:nth-child(2)").css("opacity", "0.4"); 
-        $(".section-title:nth-child(3)").css("opacity", "1"); 
-        $(".section-title:nth-child(4)").css("opacity", "0.4"); 
-    } else if (reqSlide == 4) {
-        $(".section-title:first-child").css("opacity", "0.4"); 
-        $(".section-title:nth-child(2)").css("opacity", "0.4"); 
-        $(".section-title:nth-child(3)").css("opacity", "0.4"); 
-        $(".section-title:nth-child(4)").css("opacity", "1"); 
-    }
-
-}
-
-function startCursorBlinking() {
-    var isCursorVisbile = true
+//runs the given function when the HTML document has loaded (also known as the DOM)
+$(document).ready(function(){
+    //start the cursor blinking
+    var cursor = $("#cursor");
     setInterval(function() {
-        if (isCursorVisbile) {
-            $("#cursor").css("visibility", "hidden")
+        //every 600 miliseconds, we check if our cursor
+        //has the CSS class "show", if so we remove it,
+        //if not we add it. This creates the blinking effect
+        if (cursor.hasClass("show")) {
+            cursor.removeClass("show");
         } else {
-            $("#cursor").css("visibility","visible")
+            cursor.addClass("show");
         }
-        isCursorVisbile = !isCursorVisbile
-    }, 600)
-}
-
-/*
-Background Colours:
-
-#80B1F5 BLUE
-#94D3A3 GREEN
-#E06D6F RED
-#F1C37F YELLOW
-#16703D DARKGREEN
+    }, 600);
     
-*/
+    //add the event handler to catch all click events on
+    //elements of class "bubble" within elements of class "bubble-nav"
+    //this has to be done within the DOM ready event since otherwise
+    //the $ function won't work as elements won't be ready
+    $(".bubble-nav .bubble").on("click", function() {
+        //tells us which button was clicked (number, 0-3)
+        var slideIndex = $(this).index();
+        // when slide changes, animate the slide to the side
+        var moveStr = "-" + slideIndex + "00vw";    //this makes a CSS style like -200vw or "-200% view width" to move the page over 2 view widths
+        //slide the selected page over, the CSS on the element will animate it for us (see "transition")
+        $(".pages-invisible-container").css("marginLeft", moveStr);
+        
+        //reset opacity on all bubbles
+        $(".bubble-nav .bubble").removeClass("active");
+        //set opacity full on selected bubble
+        $(this).addClass("active");
+        
+        //Note about $(this): "this" is Javascript for the variable of the function you're currently in.
+        //As you can see this comment is in a function / click event handler within the document ready event function,
+        //and "this" here refers to the click event function. $(this) passes "this" to jQuery and it figures out which
+        //element called this function (ie: which element was clicked)
 
-
+        // additionally set the faq background colour as well 
+        if ($(this).hasClass("redback")) {
+            $("#faq").css({ background: "#E06D6F" });
+            $("footer a").css({color: "#E06D6F" })
+        }
+        if ($(this).hasClass("blueback")) {
+            $("#faq").css({ background: "#80B1F5" });
+            $("footer a").css({color: "#80B1F5" })
+        }
+        if ($(this).hasClass("greenback")) {
+            $("#faq").css({ background: "#94D3A3" });
+            $("footer a").css({color: "#94D3A3" })
+        }
+        if ($(this).hasClass("yellowback")) {
+            $("#faq").css({ background: "#F1C37F" });
+            $("footer a").css({color: "#F1C37F" })
+        }
+    });
+    
+    //FAQ question accordion
+    $(".faq .question").on("click", function(){
+        //deselect all other questions
+        $(".faq .question").removeClass("active");
+        //select this question
+        $(this).addClass("active");
+        //hide all other answers
+        $(".faq .answer").removeClass("show");
+        //show our answer
+        $(this).next(".answer").addClass("show");
+    });
+});
