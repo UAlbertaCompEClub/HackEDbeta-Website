b3b057530e5f9eba76a58f9d3022864f313964ef
Arjun Kalburgi <askalburgi@users.noreply.github.com>
2017-10-23 02:00:19 -0600

    Publishing the full version to live (#3)
    * updated logo
    
    * remove unnecessary CSS, HTML, and JS
    
    also fix .logo css width
    
    * implemented a basic Colour changer also PSD for logo attached
    
    * Fixed Colour transitions
    
    FML I SHOULD BE ASLEEP
    
    * added a fixed scroll paper thingy
    
    * really bad way of scrolling stuff (but it works)
    
    super choppy, too many animations (hence kinda laggy)
    
    * fixed choppy start
    
    * fixed laggy scrolling and implemented an easy to use border interface
    
    * ugly RGB values gone, white BG added, Lorep ipsum added
    
    * fix comments
    
    * change .paper size and colours for each page
    
    * add a secondary hacked logo and added color variables for JS and CSS
    
    * oops this didn't make it through, ignore
    
    * remove old website things
    
    * savepoint temp
    
    * moved to new web template, editing next
    
    * removal of HTML elements of old site
    
    * some copy
    
    * implement font and link js file
    
    * outline and stuff
    
    * remove old index.html
    
    * create new starting point for JS
    
    * just format using beautify
    
    * Reduce CSS to like 625 lines of code
    
    (still a bit to go)
    
    * add pages
    
    * create container and fix css
    
    * implemented animation for nav switcher
    
    * integrate blinking logo
    
    * condensed font for beta
    
    * tweak beta font and remove colours and add lorem ipsum
    
    * some css changes and beginning of Register Now section
    
    * organizing css phase 1
    
    * css refactor (partial) and implemented bubble colour changes
    
    * more css changes and index.html cleanup
    
    * I just realized how fucked mobile is wahhhhh
    
    * copy and coloured buttons
    
    * Mine and Denis nearly completed site :)  (#2)
    
    * added favicon, cleaned up a bit
    
    * removed unused CSS, unused dependencies
    
    * reorganized CSS a bit
    
    * switched to SCSS instead of CSS (more readable), setup npm for live development
    
    * fixed gitignore / node_modules
    
    * more refactoring, cleaning up
    
    * fixed mobile page slideshow, commented
    
    * adding copy to the website
    
    * header and page section tweaks
    
    * sponsorship code (incomplete)
    
    * sponsor and footer rough work
    
    * added basic FAQ and styles
    
    * Added changing background colours to faq and hover effects on bubble, other css tweaks
    
    * New copy + design tweaks
    
    * LINKS
    
    * moved css code to scss where it should be
    
    * merged stashed changes
    
    * added logo locally, fixed up a bit
    
    * images and style
    
    * Fix image urls
    
    * Fix bubbles on mobile so they still overlap
    
    * Remove spacing between the header and the bubbles
    
    * Fix bubbles on tablet
    
    * Fix HackED beta title on mobile
    
    * Make the FAQ questions full width on mobile
    
    * Use white font in FAQ section
    
    * Fix price FAQ
    
    * Fix margin under pages container
    
    * Add some extra margin undert the pages section
    
    * Make sponsor images look good on desktop and mobile
    
    * Use gocubes logo with black font
    
    * Fix flex-box on safari
    
    * Extract accent color changing to a function
    
    * Fix footer alignment on mobile
    
    * Use background color in the footer
    
    * Fix FAQ for "Who organizes HackEDbeta"

---
+++ README.md
@@ -0,0 +1,12 @@
+HackedED Beta Website
+=====================
+
+Installation
+------------
+
+First install `npm`/`node` locally, then run `npm i` to install the required dependencies
+
+Development
+-----------
+
+There are only 3 main files to edit to make changes to the website: `index.html`, `main.scss`, and `main.js`. When developing the site, run `npm start` and that will start a webserver which automatically reloads when changes are made. It will also compile `main.scss` into `main.css`
--- js/main.js
+++ js/main.js
@@ -1,35 +1,69 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    startCursorBlinking()
-    chooseBackgroundColour()
-});
+function changeAccentColor(color) {
+    $(".faq").css({ background: color });
+    $("footer a").css({color: color })
+}
 
-function startCursorBlinking() {
-    var isCursorVisbile = true
+//runs the given function when the HTML document has loaded (also known as the DOM)
+$(document).ready(function(){
+    //start the cursor blinking
+    var cursor = $("#cursor");
     setInterval(function() {
-        if (isCursorVisbile) {
-            $("#cursor").css("visibility", "hidden")
+        //every 600 miliseconds, we check if our cursor
+        //has the CSS class "show", if so we remove it,
+        //if not we add it. This creates the blinking effect
+        if (cursor.hasClass("show")) {
+            cursor.removeClass("show");
         } else {
-            $("#cursor").css("visibility","visible")
+            cursor.addClass("show");
         }
-        isCursorVisbile = !isCursorVisbile
-    }, 600)
-}
+    }, 600);
 
-function chooseBackgroundColour() {
-    var colour = Math.floor(Math.random() * (5 - 1) + 1); // four colours
-    console.log(colour); 
-    switch (colour) {
-        case 1: 
-            $("body").css("backgroundColor", "#80B1F5"); 
-            break; 
-        case 2: 
-            $("body").css("backgroundColor", "#94D3A3"); 
-            break; 
-        case 3: 
-            $("body").css("backgroundColor", "#E06D6F"); 
-            break; 
-        case 4: 
-            $("body").css("backgroundColor", "#F1C37F"); 
-            break; 
-    } 
-}
+    //add the event handler to catch all click events on
+    //elements of class "bubble" within elements of class "bubble-nav"
+    //this has to be done within the DOM ready event since otherwise
+    //the $ function won't work as elements won't be ready
+    $(".bubble-nav .bubble").on("click", function() {
+        //tells us which button was clicked (number, 0-3)
+        var slideIndex = $(this).index();
+        // when slide changes, animate the slide to the side
+        var moveStr = "-" + slideIndex + "00vw";    //this makes a CSS style like -200vw or "-200% view width" to move the page over 2 view widths
+        //slide the selected page over, the CSS on the element will animate it for us (see "transition")
+        $(".pages-invisible-container").css("marginLeft", moveStr);
+
+        //reset opacity on all bubbles
+        $(".bubble-nav .bubble").removeClass("active");
+        //set opacity full on selected bubble
+        $(this).addClass("active");
+
+        //Note about $(this): "this" is Javascript for the variable of the function you're currently in.
+        //As you can see this comment is in a function / click event handler within the document ready event function,
+        //and "this" here refers to the click event function. $(this) passes "this" to jQuery and it figures out which
+        //element called this function (ie: which element was clicked)
+
+        // additionally set the faq background colour as well
+        if ($(this).hasClass("redback")) {
+            changeAccentColor("#E06D6F")
+        }
+        if ($(this).hasClass("blueback")) {
+            changeAccentColor("#80B1F5")
+        }
+        if ($(this).hasClass("greenback")) {
+            changeAccentColor("#94D3A3")
+        }
+        if ($(this).hasClass("yellowback")) {
+            changeAccentColor("#F1C37F")
+        }
+    });
+
+    //FAQ question accordion
+    $(".faq .question").on("click", function(){
+        //deselect all other questions
+        $(".faq .question").removeClass("active");
+        //select this question
+        $(this).addClass("active");
+        //hide all other answers
+        $(".faq .answer").removeClass("show");
+        //show our answer
+        $(this).next(".answer").addClass("show");
+    });
+});
