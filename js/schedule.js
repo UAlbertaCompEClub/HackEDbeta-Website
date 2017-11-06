97b8f9301cc01c9c31491d5141f823a8934db4cc
Steven Boddez <stevenboddez@gmail.com>
2017-11-05 17:33:49 -0700

    Add axis and shading

--- js/schedule.js
+++ js/schedule.js
@@ -1,3 +1,5 @@
+var hackedTime = [new Date('2017/11/18 09:00'), new Date('2017/11/19 15:00')];
+
 d3.json('../schedule.json', function(error, json) {
     var svg = d3.select('#schedule')
         .append('svg')
@@ -27,10 +29,22 @@ d3.json('../schedule.json', function(error, json) {
     // 200px per hour
     // schedule train has no brakes
     var hourScale = d3.scaleTime()
-        .domain([new Date('2017/11/18 09:00'), new Date('2017/11/19 15:00')])
+        .domain(hackedTime)
         .range([0, 6000]);
 
-    var items = svg.selectAll('.schedule-item')
+    var shading = svg.append('g')
+        .selectAll('.schedule-shade')
+        .data(d3.timeMinute.every(30).range(hackedTime[0], hackedTime[1]))
+        .enter()
+        .append('rect')
+        .classed('schedule-shade', true)
+        .attr('x', 0)
+        .attr('y', function(d) { return hourScale(d); })
+        .attr('width', svgBox.width)
+        .attr('height', 100);
+
+    var items = svg.append('g')
+        .selectAll('.schedule-item')
         .data(json)
         .enter()
         .append('g')
@@ -50,4 +64,12 @@ d3.json('../schedule.json', function(error, json) {
         .style('text-anchor', function(d) { return d.type === 'single' ? 'middle' : null; })
         .attr('x', function(d) { return boxX(d) + boxWidth(d) / 2; })
         .attr('y', function(d) { return boxY(d) + boxHeight(d) / 2; });
+
+    // y axis
+    var yAxis = d3.axisLeft(hourScale)
+        .ticks(d3.timeMinute.every(30))
+        .tickFormat(d3.timeFormat('%-H:%M'));
+
+    svg.append('g')
+        .call(yAxis);
 });
